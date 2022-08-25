c97448b2fc087830bfce15fee607b1a0e7ae6ca5
zVladimirz <tzxdlo@gmail.com>
2022-08-25 22:41:19 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,10 +8,6 @@ import ContactForm from 'components/ContactForm';
 
 function App() {
   const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
 
   const [filter, setFilter] = useState('');
@@ -23,6 +19,17 @@ function App() {
     const paeserContacts=JSON.parse(localStorage.getItem('contacts'));
     if (paeserContacts) {
       setContacts(paeserContacts);
+    }else{
+
+      setContacts(
+        [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ]
+
+      );
     }
 
   },[]);  
